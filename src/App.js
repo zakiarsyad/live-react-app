a6a4157a861a5f86f31fbc8ab7ab5011f948ca4e
Zaki Arsyad <zakiarsyad@gmail.com>
2024-07-22 08:27:06 +0700

    add dummy change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          There is a new changes to github
+          I made a change thru github PR
         </p>
         <a
           className="App-link"
