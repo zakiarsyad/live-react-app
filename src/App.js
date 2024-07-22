b136c2ff9067d1c84f220151b8506dd3d656a676
Zaki Arsyad <zakiarsyad@gmail.com>
2024-07-22 08:00:48 +0700

    edit text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          There is a new changes to github
         </p>
         <a
           className="App-link"
